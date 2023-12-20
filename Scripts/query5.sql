d6bccbe5b2532e9edd3babecf34abff12e0822d3
elevenodd <152565418+elevenodd@users.noreply.github.com>
2023-12-20 05:06:43 +0000

    modified:   Scripts/query5.sql

--- Scripts/query5.sql
+++ Scripts/query5.sql
@@ -1,13 +1,10 @@
 SELECT e.employeeId, e.firstName,e.lastName,s.salesAmount,s.soldDate,
 MAX(s.salesAmount) AS HighestSales,
 MIN(s.salesAmount) AS LowestSales, 
-count(salesId) AS Num_of_Sales
+count(salesId) AS CarsSold
 from sales s
 INNER JOIN employee e
     ON s.employeeId = e.employeeId
 WHERE s.soldDate >= date('now','start of year')
 GROUP BY e.employeeId,e.firstName,e.lastName
-ORDER BY Num_of_Sales DESC
-
-
-
+ORDER BY CarsSold DESC
